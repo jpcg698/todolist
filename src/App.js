d474104230ff7ff78d90de6338c28cb898cf94c8
Juan Pablo <juanpcortesg@gmail.com>
2021-10-14 23:21:59 -0500

    added cross text and basic date picker. faulty state management tho.

--- src/App.js
+++ src/App.js
@@ -3,56 +3,24 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import { List} from "./features/counter/List"
 import './App.css';
+import styled from "styled-components";
 
+const StyledDiv = styled.div`
+  align-items: center;
+  justify-content: center;
+  margin:auto;
+  width:60vw;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <List />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
+      <StyledDiv>
+        <List />
+      </StyledDiv>
     </div>
   );
 }
--- src/features/counter/List.js
+++ src/features/counter/List.js
@@ -11,15 +11,37 @@ import * as yup from 'yup';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from "formik"
+import styled from "styled-components"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-const RenderList = ({list,dispatch})=>{
+const ListElement = styled.div`
+  display: grid;
+  column-gap: 1rem;
+  row-gap: 3rem;
+  max-height:3em;
+  margin-bottom:1em;
+  grid-template-columns: repeat(3, minmax(300px, 1fr));
+`;
+
+
+const RenderList = ({list,dispatch,date,setDate})=>{
     if(list){
         return(
             list.map((element,i)=>(
-                <li key={i} id={i} onClick={()=>{
-                    dispatch(remove(i))}}>
+                <ListElement key={i} id={i}>
+                <div style={{margin:"auto"}} onClick={(e)=>e.target.classList.toggle("crossed-line")}>
                         {element}
-                </li>
+                </div>
+                <Button style={{margin:"auto"}} onClick={()=>{
+                    dispatch(remove(i))}}>
+                        Remove
+                </Button>
+                <label>
+                    Date to finish
+                <DatePicker selected={date} onChange={date => setDate(date)} style={{margin:"auto"}}/>
+                </label>
+                </ListElement>
             ))
         )
     }else{
@@ -38,6 +60,9 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 
 export function List() {
+
+    const [date, setDate] = useState(new Date());
+
     const formik = useFormik({
         initialValues: {
             text: '',
@@ -94,9 +119,8 @@ export function List() {
       </form>
         {/* /* )} */}
       <ul>
-          <RenderList list={list} dispatch={dispatch}/>
+          <RenderList list={list} dispatch={dispatch} date={date} setDate={setDate}/>
       </ul>
-
     </div>
   );
 }
